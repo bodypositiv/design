7a8f6937fd550e35eb2ac9ef9a591d9bcb2cce6f
dimka-miracle <mr.miracle@inbox.ru>
2013-03-04 10:56:18 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <title>Охуительный сайт</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    <div id="header">
+        <div id="header2">
+            <div id="login"></div>
+        </div>
+    </div>
+
+<div id="all">
+    <div id="left"><!-- Left Blocks -->
+        <a href="#" title="На главную" id="to_main"><img src="i/logo.jpg" alt="Boommax"></a>
+        <div class="left_block">
+            <div class="header">Коротко...</div>
+            <div class="content">
+                На сайте представлены интересные и полезные статьи, новости по разным аспектам нашей жизни.
+                Весь материал представлен преимущественно в виде советов и анонсов последних событий.
+            </div>
+        </div>
+
+    </div>
+
+    
+
+    <div id="main"><!-- Main Blocks -->
+        <h3 class="title">
+            <a href="#">В каких условиях делают ринопластику носа?</a>
+        </h3>
+
+    </div>
+
+</div>
+
+</body>
+</html>
